da4e91ff49e4ec1361646e4555818f8f31e4b9f6
unknown <>
2016-09-05 21:22:42 +0800

    第一次的版本

---
+++ java/com/example/acer/myk5/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.acer.myk5;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
+class CodeView extends Activity {
+    // 当第一次创建该Activity时回调该方法
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // 创建一个线性布局管理器
+        LinearLayout layout = new LinearLayout(this);
+        // 设置该Activity显示layout
+        super.setContentView(layout);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        // 创建一个TextView
+        final TextView show = new TextView(this);
+        // 创建一个按钮
+        Button bn = new Button(this);
+        bn.setText(R.string.ok);
+        bn.setLayoutParams(new ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT));
+        // 向Layout容器中添加TextView
+        layout.addView(show);
+        // 向Layout容器中添加按钮
+        layout.addView(bn);
+        // 为按钮绑定一个事件监听器
+        bn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                show.setText("Hello , Android , " + new java.util.Date());
+            }
+        });
+    }
+}
